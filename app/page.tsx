3436d2f9d67ba92297fda501dc6efd0d286bcd6e
Zkyarpan <karkiarpan555@gmail.com>
2024-07-22 15:23:33 +0545

    update 3 files and delete 32 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-     <section className='remove-scrollbar container my-auto'>
-      <div className='sub-container max-w-[496px]'>
-        <Image 
-         
-        />
-      </div>
-     </section>
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
   );
 }
