eb2c3094104577dd07e2fb95f6597e749c22bdba
Zkyarpan <karkiarpan555@gmail.com>
2024-07-22 22:05:35 +0545

    delete 1 file and update 4 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -12,6 +14,15 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
     </div>
