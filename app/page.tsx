1ef1e9822038ec4c9fd84a005021c0cdf06c6774
Zkyarpan <karkiarpan555@gmail.com>
2024-07-22 14:02:57 +0545

    Add assets file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click Me</Button>
+     <section className='remove-scrollbar container my-auto'>
+      <div className='sub-container max-w-[496px]'>
+        <Image 
+         
+        />
+      </div>
+     </section>
     </div>
   );
 }
